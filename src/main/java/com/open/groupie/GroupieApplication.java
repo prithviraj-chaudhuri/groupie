3abf8c4eeb5768ba003175d84cb3d54ae9e5c3d4
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2023-05-31 21:10:39 -0400

    project init

---
+++ src/main/java/com/open/groupie/GroupieApplication.java
@@ -0,0 +1,13 @@
+package com.open.groupie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupieApplication.class, args);
+	}
+
+}
